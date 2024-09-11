6b1b380f506b1906c7f8d147c79a9b58dc63a79c
Yosef Shammout <abulwail@hotmail.com>
2024-09-10 21:32:23 -0400

    Fix unused Feedbacks import and adjust App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,6 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        {/* <Feedbacks /> */}
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
